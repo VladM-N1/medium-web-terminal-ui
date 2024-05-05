2e7930d217746f01ed69a80c4fe0e87c65849118
Vlad M <vlad.milytin@gmail.com>
2024-05-05 16:53:43 +0200

    Custom output style

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ import {Term} from "./term.js";
 const settings = {};
 const selector = document.getElementById("terminal");
 const term = Term(selector, settings);
+term.print("Hello World");
+term.print("Custom Styled Text", "success");
--- term.js
+++ term.js
@@ -1,2 +1,34 @@
 export function Term(selector, settings) {
+    const termSettings = {
+        outputClass: "output",
+    }
+    if (settings) {
+
+        if (settings.outputClass) {
+            termSettings.outputClass = settings.outputClass;
+        }
+    }
+
+    function addOutputContainer() {
+        const id = "output";
+        const output = document.createElement("span");
+        output.id = id;
+        output.classList.add(termSettings.outputClass);
+        selector.appendChild(output);
+        return output;
+    }
+
+    const output = addOutputContainer();
+    function print(text, cssClass) {
+        const printElement =  document.createElement("pre");
+        printElement.innerText = text;
+        if (cssClass) {
+            printElement.classList.add(cssClass);
+        }
+        output.appendChild(printElement);
+    }
+
+    return {
+        print: print,
+    };
 }
