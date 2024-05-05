ea57d7871f44aed49c0f5911784c9d34f3d82f8b
Vlad M <vlad.milytin@gmail.com>
2024-05-05 16:55:51 +0200

    Adding input

--- term.js
+++ term.js
@@ -17,7 +17,35 @@ export function Term(selector, settings) {
         selector.appendChild(output);
         return output;
     }
+    let mirrorFromId = undefined;
+    let mirroredInput = document.createElement("span");
+    selector.appendChild(mirroredInput);
 
+    function mirrorTextFromInput(elementId) {
+        mirrorFromId = elementId;
+        const element = document.getElementById(elementId);
+        element.value = "";
+        element.focus();
+        element.addEventListener("keyup", function (event) {
+            if (mirroredInput) {
+                mirroredInput.innerText = `${event.target.value}`;
+            }
+        });
+        document.addEventListener("click", (_) => {
+            document.getElementById(mirrorFromId).focus();
+        });
+    }
+
+    function addHiddenInput() {
+        const id = "input";
+        const input = document.createElement("input");
+        input.id = id;
+        input.style["opacity"] = 0;
+        selector.appendChild(input);
+        mirrorTextFromInput(id);
+        return input;
+    }
+    addHiddenInput();
     const output = addOutputContainer();
     function print(text, cssClass) {
         const printElement =  document.createElement("pre");
